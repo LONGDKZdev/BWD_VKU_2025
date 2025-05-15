82491692d381c70a892132377c2c2d4516e79594
LONGDKZdev <longvh2549@gmail.com>
2025-05-15 22:59:49 +0700

    Add terms( điều khoản) in register

--- BWD_2025/src/js/index4.js
+++ BWD_2025/src/js/index4.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const video = document.getElementById('mainVideo');
     const nextButton = document.getElementById('nextButton');
 
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     video.play();
 
     // Xử lý sự kiện khi nhấn nút "Tiếp theo"
-    nextButton.addEventListener('click', function() {
-        window.location.href = 'index3.html';
+    nextButton.addEventListener('click', function () {
+        window.location.href = 'index1.html';
     });
 
     // Tự động chuyển trang khi video kết thúc
-    video.addEventListener('ended', function() {
-        window.location.href = 'index3.html';
+    video.addEventListener('ended', function () {
+        window.location.href = 'index1.html';
     });
 });
