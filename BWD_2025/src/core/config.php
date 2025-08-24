5bab0d46086dbe8edb50289a238a50cc97095f22
LONGDKZdev <longvh2549@gmail.com>
2025-08-24 09:56:56 +0700

    Update config.php

--- BWD_2025/src/core/config.php
+++ BWD_2025/src/core/config.php
@@ -1,3 +1,3 @@
 <?php
 define('FIREBASE_PROJECT_ID', 'song-lanh-manh');
-define('FIREBASE_API_KEY', 'AIzaSyD1CdKAIxsJn22ien9cINkbH7S46gzBM0w'); // bạn thay vào key thật
+define('FIREBASE_API_KEY', 'AIzaSyD1CdKAIxsJn22ien9cINkbH7S46gzBM0w');
